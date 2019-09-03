cbab6491eddd1ca951f7fa19958067b27727d4af
Justin Shepard <justin.shepard@gmail.com>
2019-09-03 13:14:53 -0700

    Add Base Dir Test
    Added a test to check that the base dir is correctly being prepended to
    the output filename.

---
+++ test/base-path.js
@@ -0,0 +1,28 @@
+// @flow strict
+import assert from 'assert'
+import path from 'path'
+import {
+  degenString,
+} from '../src/generator.js'
+
+import { codeGen } from '../src/base-gen.js'
+
+const generator = () => degenString()
+
+const result = codeGen(
+  __dirname,
+  '',
+  {
+    'Foo': __filename,
+  },
+  { stringify: '../src/deserializer.js', deString: '../src/deserializer.js' },
+  [
+    [ './base-path-output.js', [['strict', generator() ]] ],
+  ],
+)[0][0]
+
+assert.strictEqual(
+  result,
+  path.join(__dirname, 'base-path-output.js'),
+  "baseDir should have been prepended to the filename"
+)
