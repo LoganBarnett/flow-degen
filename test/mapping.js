f0c2a40e07bd5426d4d6b9836c8e333038835b45
Justin Shepard <justin.shepard@nwea.org>
2019-11-08 12:22:51 -0800

    Export degenMapping So It's Usable
    Added degenMapping to src/index.js so that flow-degen users can make use
    of it.

---
+++ test/mapping.js
@@ -0,0 +1,55 @@
+// @flow strict
+import assert from 'assert'
+import path from 'path'
+import {
+  degenField,
+  degenMapping,
+  degenObject,
+  degenString,
+} from '../src/index.js'
+import { runFlow } from './utils.js'
+import { codeGen } from '../src/base-gen.js'
+
+// Workaround for https://github.com/LoganBarnett/flow-degen/issues/15
+export type Container = {
+  mapping: { [string]: string },
+}
+
+const containerMetaType = { name: 'Container', typeParams: [] }
+
+const mappingContainerGenerator = () => degenObject(
+  containerMetaType,
+  [degenField('mapping', degenMapping(degenString(), degenString()))],
+  [],
+)
+
+const code = codeGen(
+  __dirname,
+  true,
+  '',
+  {
+    'Container': __filename,
+  },
+  {
+    deField: '../src/deserializer.js',
+    deMapping: '../src/deserializer.js',
+    deString: '../src/deserializer.js',
+  },
+  [
+    [
+      path.resolve(__dirname, 'mapping-output.js'), [
+        [ 'mappingContainerRefiner', mappingContainerGenerator() ],
+      ],
+    ],
+  ],
+)[0][1]
+
+runFlow(code).then((errorText) => {
+  assert.ok(
+    errorText.match(/No errors!/),
+    'Expected no errors in flow check but got errors:' + errorText,
+  )
+}).catch((e: mixed) => {
+  console.error('Error running test:', e)
+  process.exit(1)
+})
